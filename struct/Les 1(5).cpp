bc077f131815beb2a9644b286342a5d23f92d7a2
DieOnRally <84383968+AceVillOneLove@users.noreply.github.com>
2022-04-04 20:06:52 +0300

    Update Les 1(5).cpp

--- struct/Les 1(5).cpp
+++ struct/Les 1(5).cpp
@@ -1,33 +1,106 @@
+/*include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nominal[6] = {500, 100, 50, 20, 10, 5}, k, sum, n;
+    cout << "Введите количество номиналов:\n";
+    cin >> k;
+    if(k>=1 && k <=6)
+    {
+        cout << "Введите сумму\n";
+        cin >> sum;
+        n = sum;
+        for(int i = 0; i < 6; i++)
+        {
+            if (k - 1 == 0)
+            {
+                if(sum % nominal[i] == 0)
+                {
+                    n = sum / nominal[i];
+                    sum = sum % nominal[i];
+                    for(int j = 0; j < n; j++)
+                    {
+                        cout << nominal[i] << " ";
+                    }
+                    break;
+                }
+            }
+            else
+            {
+                n = sum / nominal[i];
+                sum = sum % nominal[i];
+                for(int j = 0; j < n; j++)
+                    cout << nominal[i] << " ";
+            }
+        }      
+    }
+    else
+        return 0;
+}*/
+
 #include <iostream>
 
 using namespace std;
 
-int step(int nominal[6], int &counter, int &n, int &x, int &z, int &counterZ)
+int step(int &z, int &k, int &sum, int &n, int nominal[6], int &counter, int &counterZ)
 {
-    if(n == 0)
-        return 0;
-    x = n / nominal[counter];
-    z += n / nominal[counter];
-    n = n % nominal[counter];
-    if(x > 0)
+    if (sum == 0)
+        return 0;    
+    if (k == 1)
     {
-        counterZ++;
+        if (sum % nominal[counter] == 0)
+        {
+            n += sum / nominal[counter];
+            z = sum / nominal[counter];
+            sum = sum % nominal[counter];
+            if(z > 0)
+            {
+                counterZ++;
+            }  
+            for (int i = 0; i < z; i++)
+            
+            {
+                cout << nominal[counter] << " ";
+            }
+            return 0;
+        }
+        else
+        {
+            counter++;
+            step(z, k, sum, n, nominal, counter, counterZ);
+        }
     }
-
-    for(int i = 0; i < x; i++)
-        cout << nominal[counter] << " ";
-    counter ++;
-    step(nominal, counter, n, x, z, counterZ);
-
-} 
+    else
+    {
+        n += sum / nominal[counter];
+        z = sum / nominal[counter];
+        if(z > 0)
+        {
+            counterZ++;
+            k--;
+        }
+        sum = sum % nominal[counter];
+        for (int i = 0; i < z; i++)
+        {
+            cout << nominal[counter] << " ";
+        }
+        counter++;
+        step(z, k, sum, n, nominal, counter, counterZ);
+    }
+}
+    
 
 int main()
 {
+    int nominal[6] = {500, 100, 50, 20, 10, 5}, k, sum, counter = 0, n, z, counterZ = 0;
+    cout << "Введите количество номиналов:\n";
+    cin >> k;
     printf("Введите сумму:\n");
-    int nominal[6] = {500, 100, 50, 20, 10, 5}, n, counter = 0, x = 0, z = 0, counterZ = 0;
-    cin >> n;
-    printf("Используемые номиналы: ");
-    step(nominal, counter, n, x, z, counterZ);
-    cout << "\nСуммарное использованное количество банкнот: " << z << endl;
-    cout << "Использованное количество номиналов: " << counterZ << endl;
-    }
+    cin >> sum;
+    step(z, k, sum, n, nominal, counter, counterZ);
+    cout << "\nВсего использовано купюр: " << n;
+    cout << "\nВсего использовано номиналов " << counterZ << endl;
+
+}
