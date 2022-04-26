d0119c64e510006779183fdf32a25478cbc0f910
DieOnRally <84383968+AceVillOneLove@users.noreply.github.com>
2022-04-26 21:08:57 +0300

    Update 12.cpp

--- oop/12.cpp
+++ oop/12.cpp
@@ -1,48 +1,44 @@
-#include <stdlib.h>
-#include <ctime>  
-#include <fstream>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    int N[10];
-    srand((unsigned)time(NULL));
-    double k = 0;
-    ofstream file("f.txt", ios::trunc);
-    if (file.is_open())
+	setlocale(LC_ALL, " ");
+	int sum, bufer;
+	ofstream f("f.bin", ios::binary);
+	if (f)
 	{
-		srand(time(NULL));
 		for (int i = 0; i < 10; i++)
 		{
-			N[i] = rand() % 10 - 5;
-			file << N[i] << "\t";
+			sum = rand() % 6 - 2;
+			f.write(reinterpret_cast <char*> (&sum), sizeof(int));
 		}
-		file.close();
 	}
-    ifstream infile("f.txt");
-    ofstream outfile("1.bin", ios::binary);
-
-    while (!infile.eof())
-    {
-        infile >> k;
-        if (k > 1.0)
-            outfile.write(reinterpret_cast <char*>(&k), sizeof (double));
-    }
-
-    infile.close();
-    infile.open("f.txt");
-
-    while (!infile.eof())
-    {
-        infile >> k;
-        if (k <= 1.0)
-            outfile.write(reinterpret_cast <char*>(&k), sizeof (double));
-    }
-
-    infile.close();
-    outfile.close();
-
-    return 0;
+	ifstream h("f.bin", ios::binary);
+	ifstream k("f.bin", ios::binary);
+	ofstream g("g.bin", ios::binary);
+	for (int i = 0; i < 10; i++)
+	{
+		h.read(reinterpret_cast <char*> (&bufer), sizeof(int));
+		if (bufer > 1)
+		{
+			g.write(reinterpret_cast <char*> (&bufer), sizeof(int));
+			continue;
+		}
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		k.read(reinterpret_cast <char*> (&bufer), sizeof(int));
+		if (bufer <= 1)
+		{
+			g.write(reinterpret_cast <char*> (&bufer), sizeof(int));
+			continue;
+		}
+	}
+	f.close();
+	h.close();
+	k.close();
+	g.close();
 }
