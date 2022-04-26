52544fa98a3a71063ac17fed93899ad980d4bd0d
DieOnRally <84383968+AceVillOneLove@users.noreply.github.com>
2022-04-26 20:48:32 +0300

    Update 11.cpp

--- oop/11.cpp
+++ oop/11.cpp
@@ -1,85 +1,45 @@
-﻿#include <iostream>
-#include <ctime>
+#include <iostream>
 #include <fstream>
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
 
 using namespace std;
 
-void quickSort(int* numbers, int left, int right)
-{
-	int pivot;
-	int l_hold = left;
-	int r_hold = right;
-	pivot = numbers[left];
-	while (left < right)
-	{
-		while ((numbers[right] >= pivot) && (left < right))
-			right--;
-		if (left != right)
-		{
-			numbers[left] = numbers[right];
-			left++;
-		}
-		while ((numbers[left] <= pivot) && (left < right))
-			left++;
-		if (left != right)
-		{
-			numbers[right] = numbers[left];
-			right--;
-		}
-	}
-	numbers[left] = pivot;
-	pivot = left;
-	left = l_hold;
-	right = r_hold;
-	if (left < pivot)
-		quickSort(numbers, left, pivot - 1);
-	if (right > pivot)
-		quickSort(numbers, pivot + 1, right);
-}
-
 int main()
 {
-	setlocale(LC_ALL, "");	
-	int N[10], counter = -1, bufer, z, counterZ = 0;
-	string adress = "f.txt";
-	ofstream file(adress, ios::trunc);	
-	if (file.is_open())
+	setlocale(LC_ALL, " ");
+	int bufer;
+	ofstream f("f.txt");
+	if (f)
 	{
 		srand(time(NULL));
 		for (int i = 0; i < 10; i++)
 		{
-			N[i] = rand() % 10 - 5;
-			file << N[i] << "\t";
+			f << rand() % 6 - 2 << "\t";
 		}
-		file.close();
 	}
-	ifstream newfile;
-	newfile.open(adress);
-	if (newfile.is_open())
+	f.close();
+	ifstream h("f.txt");	
+	ifstream k("f.txt");
+	ofstream g("g.txt");
+	for (int i = 0; i < 10; i++)
 	{
-		while(!newfile.eof())
+		h >> bufer;
+		if (bufer > 1)
 		{
-			counter++;
-			newfile >> N[counter];			
-			if (counter > 8)
-				break;
+			g << bufer << "\t";
+			continue;
 		}
-		newfile.close();
-	}	
-	string newadress = "g.txt";
-	file.open(newadress);
-	if (file.is_open())
-	{
-		file << "Вещественные числа из f.txt:\n";
-		for (int i = 0; i <= counter; i++)
-			file << N[i] << "\t";
-		quickSort(N, 0, counter);
-		file << "\nОтсортированный список f.txt:\n";
-		for (counter; counter >= 0; counter--)
-			file << N[counter] << "\t";
-		newfile.close();
 	}
+	for (int i = 0; i < 10; i++)
+	{
+		k >> bufer;
+		if (bufer <= 1)
+		{
+			g << bufer << "\t";
+			continue;
+		}
+	}
+	f.close();
+	h.close();
+	k.close();
+	g.close();
 }
